bca014c83f0fc059bcc80c51a010f7648b352e2d
Cherry Huang <chuang031@gmail.com>
2022-11-07 15:32:49 -0800

    added demo user

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from "react-router-dom";
 import * as sessionActions from '../../store/session';
 import './Navigation.css'
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-  
+  const sessionUser = useSelector(state => state.session.user);
 const history= useHistory()
   
   useEffect(() => {
@@ -20,6 +20,7 @@ const history= useHistory()
   
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
+
 
   const logout = (e) => {
     e.preventDefault();
@@ -41,6 +42,8 @@ history.push('/')
           <li>
             <button className='logout_button' onClick={logout}>Log Out</button>
           </li>
+
+        
         </ul>
       
     </>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -8,14 +8,21 @@ import logo from "../../images/logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSolid, faBars , faUser} from '@fortawesome/free-solid-svg-icons'
 import { useRef } from "react";
-
+import * as sessionActions from '../../store/session';
 function Navigation({ isLoaded }) {
   const dispatch = useDispatch();
-
+  const currentUser = useSelector((state) => state.session.user);
   const [open, setOpen] = useState(false)
   const sessionUser = useSelector((state) => state.session.user);
 
-  
+  const demoUser=(e)=>{
+    e.preventDefault()
+        const credential = 'Demo-lition'
+        const password = 'password'
+    
+        return dispatch(sessionActions.login({ credential, password }))
+       
+      }
   
   useEffect(()=>{
     let handleSubmit = (e)=>{
@@ -57,7 +64,14 @@ function Navigation({ isLoaded }) {
           <ul className= 'drop-menu'>
             <li className='dropdown-items'>
             {isLoaded }
-            {sessionLinks}</li>
+            {sessionLinks}
+         
+            {!currentUser &&(
+            <button className='demo-button' onClick={demoUser}>Demo User</button>
+            )}
+            </li>
+            
+
           </ul>
           </div>
         </nav>
